b8e59a698161efe8f9274c09d1f9e68325917f14
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-10-04 20:32:36 +0200

    Frontend - books list and add/edit/delete forms

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "book_notes",
-  password: "", //delete!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  password: "",
   port: 5432,
 });
 
